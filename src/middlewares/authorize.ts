aa49f882f904fa5741752f4dad796374b4543237
Mostafiz002 <mo.mahin4@gmail.com>
2026-01-11 17:16:44 +0600

    feat: user validation middleware added

---
+++ src/middlewares/authorize.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+
+export const authorize =
+  (roles: string[]) => (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user;
+
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    if (!roles.includes(user.role)) {
+      return res.status(403).json({ message: "Forbidden: Access denied" });
+    }
+
+    next();
+  };
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { UserController } from "./user.controller";
+import { authorize } from "../../middlewares/authorize";
+import catchAsync from "../../shared/catchAsync";
 
 const router = Router();
 
 router.post("/", UserController.createUser);
-router.get("/", UserController.getAllUsers);
-router.get("/:id", UserController.getSingleUser);
+router.get("/", authorize(["ADMIN"]), catchAsync(UserController.getAllUsers));
+router.get("/:id", authorize(["ADMIN"]), catchAsync(UserController.getSingleUser));
 
 export const UserRouter = router;
